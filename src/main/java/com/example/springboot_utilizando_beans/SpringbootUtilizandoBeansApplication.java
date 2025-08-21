67b3cfd0a3bb61f754297a802330527676db0c9a
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-21 09:40:50 -0300

    first commit

---
+++ src/main/java/com/example/springboot_utilizando_beans/SpringbootUtilizandoBeansApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_utilizando_beans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUtilizandoBeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUtilizandoBeansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_utilizando_beans/SpringbootUtilizandoBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_utilizando_beans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUtilizandoBeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
